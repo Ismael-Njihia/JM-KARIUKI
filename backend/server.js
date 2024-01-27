ff83358b32a36ef7c75a9bb50ad1dcd96def58f0
Ismael njihia <engishmaelnjihia@gmail.com>
2024-01-27 19:12:46 +0300

    Worked on the Health Records

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import userRoute from './route/userRoute.js';
 import appointRoute from './route/appointRoute.js';
+import healthRoute from './route/healthRoute.js';
 import cookieParser from 'cookie-parser';
 
 const app = express();
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use('/api/users', userRoute);
 app.use('/api/appointments', appointRoute);
+app.use('/api/health', healthRoute);
 
 
 app.listen(PORT, () => {
