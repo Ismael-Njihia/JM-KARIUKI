d0c6a5f42eca671202631ec22375674d7b7dcee0
Ismael njihia <engishmaelnjihia@gmail.com>
2024-02-06 13:05:07 +0300

    Creation of medical records which are assocuated with doctor completing an appointment

--- backend/controller/medicalController.js
+++ backend/controller/medicalController.js
@@ -11,6 +11,82 @@ const getAllmedical = asyncHandler(async(req, res)=>{
     res.json(medical)
 })
 
+//POST CREATE MEDICAL RECORD
+//POST /api/medical
+//private
+const createMedical = asyncHandler(async(req, res)=>{
+    const userId = req.user.userId;
+    const {appointId, test_results, prescriptions} = req.body;
+
+    //verufy if appointment exists
+    const appointment = await prisma.appointment.findUnique({
+        where: {
+            appointId: appointId
+        }
+    })
+
+    if(!appointment){
+        res.status(404).json({message: 'Appointment not found'})
+        return
+    }
+    //get the doctorId from the appointment
+   // const doctorId = appointment.doctorId;
+   // if(doctorId !== userId){
+   //     res.status(401).json({message: 'You had not been Booked for this appointment'})
+  //      return
+  //  }
+    //create medical record
+    const medicalId = randomGenerator().toString();
+    const timestamp = new Date().toISOString();
+
+    //check if medical record exists
+    const medicalExists = await prisma.medicalInfo.findUnique({
+        where: {
+            medicalId
+        }
+    })
+
+    if(medicalExists){
+        res.status(400).json({message: 'Medical record already exists'})
+        return
+    }
+
+    //create medical record
+    const medical = await prisma.medicalInfo.create({
+        data: {
+            medicalId,
+            appointId,
+            test_results,
+            prescriptions,
+            timestamp
+        }
+    })
+    if(medical){
+        //update appointment status as completed
+        const updateAppointment = await prisma.appointment.update({
+            where: {
+                appointId: appointId
+            },
+            data: {
+                appointStatus: 'completed'
+            }
+        })
+
+        res.status(201).json({
+            medical,
+            updateAppointment,
+            message: 'Medical record created successfully'
+        })
+    }
+
+  
+   if(!medical){
+       res.status(400).json({message: 'Medical record creation failed'})
+       return
+   }
+
+})
+
 
 //export
-export {getAllmedical}
+export {getAllmedical, createMedical}
--- backend/route/medicalRoute.js
+++ backend/route/medicalRoute.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
 import {admin, doctor, auth} from "../middleware/authMiddleware.js"
-import { getAllmedical } from '../controller/medicalController.js';
+import { getAllmedical, createMedical } from '../controller/medicalController.js';
 
 router.route('/').get(auth, getAllmedical);
+router.route('/').post(auth, doctor, createMedical);
 
 export default router;
